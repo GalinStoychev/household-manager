399b0d337712b498ce550dc39e4941aaf7d85c17
GalinStoychev <stoychev.galin@gmail.com>
2017-03-10 20:08:14 +0200

    web forms view engine removed

--- HouseholdManager/HouseholdManager.Web/Global.asax.cs
+++ HouseholdManager/HouseholdManager.Web/Global.asax.cs
@@ -12,6 +12,9 @@ namespace HouseholdManager.Web
     {
         protected void Application_Start()
         {
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
