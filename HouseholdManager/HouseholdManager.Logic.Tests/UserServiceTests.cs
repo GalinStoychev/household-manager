797146c4df19e24a291b7fbec0be4f2e1607394a
GalinStoychev <stoychev.galin@gmail.com>
2017-03-23 16:22:34 +0200

    UserServiceTests updated with more tests

--- HouseholdManager/HouseholdManager.Logic.Tests/UserServiceTests.cs
+++ HouseholdManager/HouseholdManager.Logic.Tests/UserServiceTests.cs
@@ -12,13 +12,19 @@ namespace HouseholdManager.Logic.Tests
     [TestFixture]
     public class UserServiceTests
     {
+        private Mock<IUnitOfWork> unitOfWorkMock;
+        private Mock<IRepository<User>> userRepoMock;
+
+        [SetUp]
+        public void SetUpMocks()
+        {
+            this.unitOfWorkMock = new Mock<IUnitOfWork>();
+            this.userRepoMock = new Mock<IRepository<User>>();
+        }
+
         [Test]
         public void UserService_ShouldThrowNewArgumentException_WhenUnitOfWorkIsNull()
         {
-            //Arrange
-            var userRepoMock = new Mock<IRepository<User>>();
-
-            //Act
             //Assert
             Assert.Throws<ArgumentNullException>(() => new UserService(null, userRepoMock.Object));
         }
@@ -26,10 +32,6 @@ namespace HouseholdManager.Logic.Tests
         [Test]
         public void UserService_ShouldThrowNewArgumentException_WhenUserRepositoryIsNull()
         {
-            //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-
-            //Act
             //Assert
             Assert.Throws<ArgumentNullException>(() => new UserService(unitOfWorkMock.Object, null));
         }
@@ -38,47 +40,41 @@ namespace HouseholdManager.Logic.Tests
         public void UserRepository_ShouldCallGetByIdOnce_WhenAddHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var user = new User();
-            userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(user);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(user);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.AddHousehold(null, null);
 
             //Assert
-            userRepoMock.Verify(x => x.GetById(It.IsAny<object>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<object>()), Times.Once);
         }
 
         [Test]
         public void UnitOfWork_ShouldCallCommitOnce_WhenAddHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userMock = new Mock<User>();
             userMock.Setup(x => x.Households).Returns(new List<Household>());
-            userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userMock.Object);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userMock.Object);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.AddHousehold(null, null);
 
             //Assert
-            unitOfWorkMock.Verify(x => x.Commit(), Times.Once);
+            this.unitOfWorkMock.Verify(x => x.Commit(), Times.Once);
         }
 
         [Test]
         public void UserHousehold_ShouldBeIncreasedByOne_WhenAddHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userMock = new Mock<User>();
             userMock.Setup(x => x.Households).Returns(new List<Household>());
             var expeceted = userMock.Object.Households.Count + 1;
-            userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userMock.Object);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userMock.Object);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
@@ -92,11 +88,9 @@ namespace HouseholdManager.Logic.Tests
         public void UserCurrentHousehold_ShouldBeChangesWithTheOncePassed_WhenAddHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
             var householdStub = new Household("_", " _", new byte[0]);
-            userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
@@ -110,28 +104,24 @@ namespace HouseholdManager.Logic.Tests
         public void UserRepository_ShouldCallGetFirstOnce_WhenGetCurrentHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
-            userRepoMock.Setup(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.GetCurrentHousehold("_");
 
             //Assert
-            userRepoMock.Verify(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>()), Times.Once);
         }
 
         [Test]
         public void GetCurrentHousehold_ShouldReturnInstanseOfHousehold_WhenCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
             userStub.SetCurrentHousehold(new Household("_", "_", new byte[0]));
-            userRepoMock.Setup(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetFirst(It.IsAny<Expression<Func<User, bool>>>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
@@ -145,27 +135,23 @@ namespace HouseholdManager.Logic.Tests
         public void UserRepository_ShouldCallGetByIdOnce_WhenGetUserInfoIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
-            userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.GetUserInfo("_");
 
             //Assert
-            userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
         public void GetUserInfo_ShouldReturnInstanceOfUser_WhenCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
-            userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
@@ -179,72 +165,184 @@ namespace HouseholdManager.Logic.Tests
         public void UserRepository_ShouldCallGetByIdOnce_WhenFirstOverloadOfSetCurrentHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
-            userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.SetCurrentHousehold(new Guid(), "_");
 
             //Assert
-            userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
         public void unitOfWork_ShouldCallCommitOnce_WhenFirstOverloadOfSetCurrentHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
             var householdStub = new Household("_", "_", new byte[0]);
             userStub.Households.Add(householdStub);
-            userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.SetCurrentHousehold(householdStub.Id, "_");
 
             //Assert
-            userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
         public void UserRepository_ShouldCallGetByIdOnce_WhenSecondOverloadOfSetCurrentHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
-            userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.SetCurrentHousehold("_", "_");
 
             //Assert
-            userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
         }
 
         [Test]
-        public void unitOfWork_ShouldCallCommitOnce_WhenSecondOverloadOfSetCurrentHouseholdIsCalled()
+        public void UnitOfWork_ShouldCallCommitOnce_WhenSecondOverloadOfSetCurrentHouseholdIsCalled()
         {
             //Arrange
-            var unitOfWorkMock = new Mock<IUnitOfWork>();
-            var userRepoMock = new Mock<IRepository<User>>();
             var userStub = new User();
             var householdStub = new Household("_", "_", new byte[0]);
             userStub.Households.Add(householdStub);
-            userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<string>())).Returns(userStub);
             var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
 
             //Act
             userService.SetCurrentHousehold(householdStub.Name, "_");
 
             //Assert
-            userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<string>()), Times.Once);
+        }
+
+        [Test]
+        public void GetAll_ShouldReturnIEnumerableOfUsers_WhenCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+
+            // Act
+            var result = userService.GetAll();
+
+            // Assert
+            Assert.IsInstanceOf<IEnumerable<User>>(result);
+        }
+
+        [Test]
+        public void UserRepository_ShouldCallGetAllOnce_WhenGetAllIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+
+            // Act
+            var result = userService.GetAll();
+
+            // Assert
+            this.userRepoMock.Verify(x => x.GetAll(
+                It.IsAny<Expression<Func<User, bool>>>(),
+                It.IsAny<Expression<Func<User, User>>>(),
+                It.IsAny<Expression<Func<User, object>>>()),
+                Times.Once);
+        }
+
+        [Test]
+        public void UserRepository_ShouldCallGetByIdOnce_WhenGetAllIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+            var userStub = new User();
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+
+            // Act
+            userService.UpdateUserInfo("_", "_", "_", "_");
+
+            // Assert
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<object>()), Times.Once);
+        }
+
+        [Test]
+        public void UserRepository_ShouldCallUpdateOnce_WhenGetAllIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+            var userStub = new User();
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+
+            // Act
+            userService.UpdateUserInfo("_", "_", "_", "_");
+
+            // Assert
+            this.userRepoMock.Verify(x => x.Update(It.IsAny<User>()), Times.Once);
+        }
+
+        [Test]
+        public void UnitOfWork_ShouldCallCommitOnce_WhenGetAllIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+            var userStub = new User();
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+
+            // Act
+            userService.UpdateUserInfo("_", "_", "_", "_");
+
+            // Assert
+            this.unitOfWorkMock.Verify(x => x.Commit(), Times.Once);
+        }
+
+        [Test]
+        public void UserRepository_ShouldCallGetByIdOnce_WhenDeleteIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+            var userStub = new User();
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+
+            // Act
+            userService.Delete("_", true);
+
+            // Assert
+            this.userRepoMock.Verify(x => x.GetById(It.IsAny<object>()), Times.Once);
+        }
+
+        [Test]
+        public void UserRepository_ShouldCallUpdateOnce_WhenDeleteIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+            var userStub = new User();
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+
+            // Act
+            userService.Delete("_", true);
+
+            // Assert
+            this.userRepoMock.Verify(x => x.Update(It.IsAny<User>()), Times.Once);
+        }
+
+        [Test]
+        public void UnitOfWork_ShouldCallCommitOnce_WhenDeleteIsCalled()
+        {
+            // Arrange
+            var userService = new UserService(unitOfWorkMock.Object, userRepoMock.Object);
+            var userStub = new User();
+            this.userRepoMock.Setup(x => x.GetById(It.IsAny<object>())).Returns(userStub);
+
+            // Act
+            userService.Delete("_", true);
+
+            // Assert
+            this.unitOfWorkMock.Verify(x => x.Commit(), Times.Once);
         }
     }
 }
